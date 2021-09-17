868622d28bd7d071b18674d35713d5d22d45edce
wind <wind.zw.zheng@foxconn.com>
2021-09-17 15:48:00 +0800

    update

--- src/main/java/com/zzw/controller/UserController.java
+++ src/main/java/com/zzw/controller/UserController.java
@@ -26,6 +26,7 @@ public class UserController {
                 return "insert success";
             }
             return "insert failed";
+            //123
         } catch (Exception e) {
             return "error";
         }
